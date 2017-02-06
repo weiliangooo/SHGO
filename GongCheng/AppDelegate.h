1f824f80f3d513969acaae8fa6d40e19aced20dc
Alen <weiliangooo@163.com>
2017-02-06 13:41:35 +0800

    Initial Commit

---
+++ GongCheng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GongCheng
+//
+//  Created by Alen on 2017/2/6.
+//  Copyright © 2017年 Alen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
